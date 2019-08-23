ea510f162a1469c2e4fe1480f5131d8cec0aa53f
Sailaja_Chillale <Sailaja_Chillale@Dell.com>
2019-08-23 08:48:27 +0530

    First commit

--- VirtualGit/Models/MyConfig.cs
+++ VirtualGit/Models/MyConfig.cs
@@ -6,6 +6,7 @@ namespace VirtualGit.Models
         public class MyConfig
         {
             public string ConnectionOS { get; set; }
+
             public int ConnectionCS { get; set; }
         }
     }
